23ce42feefc3634bad3aef01fe799cbf74a38695
Boanerges1996 <samsonnkrumah253@gmail.com>
2021-05-01 23:06:02 +0530

    Added recursive property to public folder

--- src/functions/public.js
+++ src/functions/public.js
@@ -3,16 +3,22 @@ import path from "path";
 
 const createPublicFolder = async () => {
   // 1. create public folder
-  await fs.mkdirSync(path.join(process.cwd(), "public"));
+  await fs.mkdirSync(path.join(process.cwd(), "public"), { recursive: true });
 
   // 2. Change working directory into public folder
   await process.chdir(path.join(process.cwd(), "public"));
 };
 
 const createDefaultPublicFolders = async () => {
-  await fs.mkdirSync(path.join(process.cwd(), "images"));
-  await fs.mkdirSync(path.join(process.cwd(), "videos"));
-  await fs.mkdirSync(path.join(process.cwd(), "audios"));
+  await fs.mkdirSync(path.join(process.cwd(), "images"), {
+    recursive: true,
+  });
+  await fs.mkdirSync(path.join(process.cwd(), "videos"), {
+    recursive: true,
+  });
+  await fs.mkdirSync(path.join(process.cwd(), "audios"), {
+    recursive: true,
+  });
 };
 
 export { createPublicFolder, createDefaultPublicFolders };
